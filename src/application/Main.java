8770426ec21a71c68819651ae9b976d9c29b6efa
BernardoLopes <bernardomak42@gmail.com>
2024-01-02 17:31:21 -0300

    Lendo arquivo com FileReader e BufderedReader

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,8 @@ package application;
 
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
