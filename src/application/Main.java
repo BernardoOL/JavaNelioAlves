2639d5760700455b758c0ea1ea6fea793f287abc
BernardoLopes <bernardomak42@gmail.com>
2024-01-02 17:21:39 -0300

    Lendo arquivo básico (Foi criado um arquivo txt com o nome in)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,29 @@
 package application;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        File file = new File("C:\\temp\\in.txt");//Colocar o caminho do seu arquivo
+        Scanner sc = null;
 
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        finally {
+            if(sc != null){
+                sc.close();
+            }
+        }
 
     }
 }
