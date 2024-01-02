0fb1a2650d3ae5b8a54bdca465a417e91957e264
BernardoLopes <bernardomak42@gmail.com>
2024-01-02 17:44:54 -0300

    Criando e modificando o arquivo usando FileWriter e BufferedWriter

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,39 @@
 package application;
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
 
-        String path = "C:\\temp\\in.txt";//Colocar o caminho do seu arquivo
+        String path = "C:\\temp\\out.txt";//Colocar o caminho do seu arquivo
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line = br.readLine();
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){//Tire o ", true" caso queira recriar o arquivo ao invés de adicionar conteúdo dentro dele
 
-            while(line != null){
-                System.out.println(line);
-                line = br.readLine();
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
             }
 
         }catch (IOException e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
+
     }
 }
+
+//LER O ARQUIVO
+//        String path = "C:\\temp\\in.txt";//Colocar o caminho do seu arquivo
+//
+//        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+//            String line = br.readLine();
+//
+//            while(line != null){
+//                System.out.println(line);
+//                line = br.readLine();
+//            }
+//
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
