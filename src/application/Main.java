e8220f8ac180be655a5ad2b1e95c616fbf5abad5
BernardoLopes <bernardomak42@gmail.com>
2024-01-03 11:20:40 -0300

    Atividade de arquivos: Lendo um arquivo .csv e criando um novo arquivo modificado a partir dos valores do arquivo lido

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,61 @@
 package application;
 
 
+import model.Produto;
+
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a folder path: ");
-        String strPath = sc.nextLine();
+        List<Produto> list = new ArrayList<>();
 
-        File path = new File(strPath);
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
 
-        System.out.println("getName: " + path.getName());
-        System.out.println("getParent: " + path.getParent());
-        System.out.println("getPath: " + path.getPath());
+        File sourceFile = new File(sourceFileStr);
+
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean sucecess = new File(sourceFolderStr + "\\out").mkdir();
+
+        System.out.println("Directory created: " + sucecess);
+
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null){
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Produto(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+
+                for(Produto item : list){
+                    bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + " CREATED!");
+
+            }catch (IOException e){
+                System.out.println("Erro writing file: " + e.getMessage());
+            }
+
+        }catch (Exception e){
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
